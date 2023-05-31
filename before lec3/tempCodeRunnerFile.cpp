d629f009ffa7727c6a3c4e2526157a022a4e4239
Sanjay Raghav <129708157+AttackOnCoding@users.noreply.github.com>
2023-05-31 18:20:44 +0530

    Add files via upload

---
+++ before lec3/tempCodeRunnerFile.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+   int a;
+   cin>>a;
+     
+
+     if (a>0)
+     {
+        cout<<"a is +ve"<<endl;
+
+
+     }
+
+     else{
+
+        cout<<"a is-ve"<<endl;
+     }
+
+   
+   
+ }
+
+
+
